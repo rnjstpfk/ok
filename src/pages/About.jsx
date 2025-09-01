f963d5e9e504a6d55a904aa27c062346ed17d354
sera <sera9898@naver.com>
2025-09-01 17:27:48 +0900

    about

--- src/pages/About.jsx
+++ src/pages/About.jsx
@@ -1,8 +1,71 @@
+import "./About.scss";
+
+// 🔸 에셋 (프로젝트 경로에 맞게 수정)
+import artMain from "../assets/about/ZakShaiLou.avif";   // 좌측 큰 일러스트
+import artChip from "../assets/about/MIRRORS_EDIT.avif";   // 우측 작은 카드 이미지
+
 export default function About() {
   return (
-    <div>
-      <h1>About Page</h1>
-      <p>This is the Eatables page.</p>
-    </div>
+    <main className="about-page">
+      {/* 상단 얇은 바 - 전체 폭 */}
+      <div className="page-banner">LET’S TALK ABOUT ME.</div>
+
+      {/* 히어로 */}
+      <section className="hero">
+        <div className="container">
+          <h1 className="hero-title">OK<br/>WHO?</h1>
+
+          <p className="hero-sub">
+            Hi, it’s us, OK Drugs. Nice to meet you.
+            <br/>
+            If you’re here it’s probably because you want to know
+            <br/>
+            more about us. Weird. Still, we’ll indulge you.
+          </p>
+        </div>
+      </section>
+
+      {/* 본문 레이아웃 */}
+      <section className="container body">
+        <div className="grid">
+          {/* 왼쪽: 큰 일러스트 */}
+          <figure className="main-art">
+            <img src={artMain} alt="OK Drugs founders illustration" />
+          </figure>
+
+          {/* 오른쪽: 텍스트 블록 */}
+          <div className="copy">
+            <p>
+              After a decade of working in the cannabis industry we finally said OK Drugs,
+              and the response was an astounding “WTF, is OK Drugs?” but that just made us
+              love it more. A funny thing happened once we’d escaped the corporate hamster wheel /
+              rat cage / rodent metaphor and stopped to appreciate the epic micro moments that surrounded us.
+            </p>
+
+            <p>
+              It’s a sort of supplement for self improvement… a magic bullet pointed straight at mindfulness…
+              something that helps shape the way you view the world around you.
+            </p>
+
+            {/* 우측 칩 이미지 */}
+            <figure className="chip">
+              <img src={artChip} alt="OK Drugs capsule pattern" />
+            </figure>
+
+            <p className="small">
+              If you really want to get down to it, we’re just your friendly neighborhood
+              advocates for alternative head spaces.
+            </p>
+
+            <p className="signoff">
+              Love,<br/>
+              Zak, Shai and Lou.
+            </p>
+          </div>
+        </div>
+        
+      </section>
+      <div className="footer-banner">Not sure where you are, but you are definitely here.</div>
+    </main>
   );
 }
--- src/pages/Wearables.jsx
+++ src/pages/Wearables.jsx
@@ -1,8 +1,97 @@
+import "./Wearables.scss";
+
+// 🔸 이미지 에셋 (프로젝트 경로에 맞게 수정)
+// 히어로 왼쪽 큰 이미지 (모자+선인장)
+import hatImg from "../assets/wearables/Yacht_it_final.avif";
+// 중간 콜라주 3장 + 우측 두 장
+import collage1 from "../assets/wearables/Screen Shot 2020-07-04 at 1_59_58 PM.avif";
+import collage2 from "../assets/wearables/Screen Shot 2020-07-04 at 2_00_52 PM.avif";
+import collage3 from "../assets/wearables/Screen Shot 2020-07-04 at 2_01_42 PM.avif";
+import collage4 from "../assets/wearables/Eye.avif";
+import collage5 from "../assets/wearables/Screen Shot 2020-07-04 at 2_00_29 PM.avif";
+
 export default function Wearables() {
   return (
-    <div>
-      <h1>Wearables Page</h1>
-      <p>This is the Wearables page.</p>
-    </div>
+    <main className="wearables-page">
+      {/* 상단 얇은 바 - 전체 폭 */}
+      <div className="page-banner">
+        NOT SURE WHERE YOU ARE BUT YOU ARE DEFINITELY HERE
+      </div>
+
+      <section className="container hero">
+        <h1 className="title">WEARABLES</h1>
+        <p className="subtitle">
+          Happiness is a state of mind, laughter is a drug. I get it, now get me some.
+          <br />
+          We heard you wanted wearables to go with your eatables.
+        </p>
+
+        <div className="hero-grid">
+          {/* 좌: 큰 이미지 */}
+          <figure className="hero-image">
+            <img src={hatImg} alt="OK Drugs Yacht Club Hat on cactus" />
+          </figure>
+
+          {/* 우: 제품 정보 블럭 */}
+          <div className="hero-info">
+            <h2 className="product-name">
+              <span>YACHT CLUB</span>
+              <span>HAT</span>
+            </h2>
+
+            <a className="stock" href="#">Out of Stock</a>
+
+            <button className="btn-primary" disabled>
+              Buy Now
+            </button>
+          </div>
+        </div>
+      </section>
+
+      {/* 배경색 구분 섹션 */}
+      <section className="band">
+        <div className="container collage">
+          <div className="cards">
+            <img className="card tilt-l" src={collage1} alt="Community photo 1" />
+            <img className="card center" src={collage2} alt="Community photo 2" />
+            <img className="card tilt-r" src={collage3} alt="Community photo 3" />
+          </div>
+
+          <div className="side">
+            <img className="chip top" src={collage4} alt="Small photo 1" />
+            <div className="follow">
+              {/* <span className="eye" aria-hidden>👁</span> */}
+              <p>
+                Follow <b>@okdrugsco</b>
+                <br />
+                #okdrugs #claritytherapy
+              </p>
+            </div>
+            <img className="chip bottom" src={collage5} alt="Small photo 2" />
+          </div>
+        </div>
+      </section>
+
+      <section className="street">
+        <h2 className="street-title">
+          <span>WORD ON</span>
+          <span>THE STREET</span>
+        </h2>
+
+        <div className="street-copy">
+          <p>
+            Follow us on instagram <b>@okdrugsco</b>.
+            <br />
+            Tag us in a delicious post of you and
+            <br />
+            your OK Drugs merch
+            <br />
+            and we will feature them right here,
+            <br />
+            right now. <b>#okdrugs</b>
+          </p>
+        </div>
+      </section>
+    </main>
   );
 }
